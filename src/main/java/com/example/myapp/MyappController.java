ca416da4ed6388e92c1a0e335c3bf389d8a10fef
sugbhatt <31274736+sugbhatt@users.noreply.github.com>
2018-09-28 17:22:39 +0530

    Update MyappController.java

--- src/main/java/com/example/myapp/MyappController.java
+++ src/main/java/com/example/myapp/MyappController.java
@@ -13,7 +13,7 @@ public class MyappController {
 
     @RequestMapping("/")
     public String greeting() throws UnknownHostException{
-        return "Hello boot! Date: " + new SimpleDateFormat("yyyy-MM-dd:HH.mm.ss").format(new Date()) + " from Host: " + InetAddress.getLocalHost().getHostName(); 
+        return "Hello jenkins! Date: " + new SimpleDateFormat("yyyy-MM-dd:HH.mm.ss").format(new Date()) + " from Host: " + InetAddress.getLocalHost().getHostName(); 
     }
   
 }
